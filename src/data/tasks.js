100421755571d74389c04924bc317dff0973ff7a
Jeremías <jeremiias122002@gmail.com>
2022-11-24 19:54:22 +0100

    first commit

---
+++ src/data/tasks.js
@@ -0,0 +1,17 @@
+export const tasks = [
+    {
+        id: 0,
+        title: 'mi primer tarea',
+        description: 'mi primera web'
+    },
+    {
+        id: 1,
+        title: 'mi segunda tarea',
+        description: 'mi segunda web'
+    },
+    {
+        id: 2,
+        title: 'mi tercera tarea',
+        description: 'mi tercera web'
+    }
+]
